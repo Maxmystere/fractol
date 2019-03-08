600e25dd377123fe880e01e1744ebd00e3b16d9d
Maxence GRAB <magrab@e3r6p17.42.fr>
2019-03-08 02:42:58 +0100

    Add ship

--- includes/fractol.h
+++ includes/fractol.h
@@ -37,8 +37,8 @@
 typedef struct	s_dot{
 	double	x;
 	double	y;
-	int sx;
-	int sy;
+	int		sx;
+	int		sy;
 }				t_dot;
 
 typedef struct	s_pos{
@@ -90,7 +90,7 @@ typedef struct	s_th{
 ** GPU Stuff
 */
 
-# define MANDELDRAWER "typedef struct  s_frcl{double camx;double camy;\
+# define MANDELDRAWER "typedef struct s_frcl{double camx;double camy;\
 double  camz;int winsx;int winsy;int iter;int color;}t_frcl;\
 __kernel void color(__global const t_frcl *in, __global unsigned int *out)\
 {const uint index = get_global_id(0);const double x = index % in->winsx;\
@@ -102,27 +102,42 @@ i = 0;while ((new_r * new_r + new_i * new_i) < 4.0 && i < in->iter){\
 old_r = new_r;old_i = new_i;new_r = old_r * old_r - old_i * old_i + pr;\
 new_i = 2.0 * old_r * old_i + pi;i++;}out[index] = i * in->color;}"
 
+# define SHIPDRAWER "typedef struct s_frcl{double camx;double camy;\
+double  camz;int winsx;int winsy;int iter;int color;}t_frcl;\
+__kernel void color(__global const t_frcl *in, __global unsigned int *out)\
+{const uint index = get_global_id(0);const double x = index % in->winsx;\
+const double y = index / in->winsx;\
+const double pr = x / in->camz - (in->camx * 4.0 + in->winsx)/ (in->camz * 2);\
+const double pi = y / in->camz - (in->camy * 4.0 + in->winsy)/ (in->camz * 2);\
+double new_r;double new_i;double old_r;double old_i;int i;new_r = 0;new_i = 0;\
+i = 0;while ((new_r * new_r + new_i * new_i) < 4.0 && i < in->iter){\
+old_r = (new_r < 0 ? -new_r : new_r);old_i = (new_i < 0 ? -new_i : new_i);\
+new_r = old_r * old_r - old_i * old_i + pr;\
+new_i = 2.0 * old_r * old_i + pi;i++;}out[index] = i * in->color;}"
+
 typedef struct	s_gpu{
-	cl_int 					err;
-	cl_platform_id			 ptm;
-	cl_uint					 ptms;
-	cl_device_id 			dvc;
-	cl_uint					 dvcs;
+	cl_int					err;
+	cl_platform_id			ptm;
+	cl_uint					ptms;
+	cl_device_id			dvc;
+	cl_uint					dvcs;
 	size_t					srcsize;
 	cl_program				prog;
 	cl_kernel				k_color;
 	cl_context_properties	properties[3];
 }				t_gpu;
 
-typedef struct  s_frcl{
-    double  camx;
-    double  camy;
-    double  camz;
-    int     winsx;
-    int     winsy;
+typedef struct	s_frcl{
+	double	camx;
+	double	camy;
+	double	camz;
+	double	jx;
+	double	jy;
+	int		winsx;
+	int		winsy;
 	int		iter;
 	int		color;
-}               t_frcl;
+}				t_frcl;
 
 int				gpu_calcul(t_frcl param, t_fdf *fdf, const char *src);
 
--- srcs/draw_ship.c
+++ srcs/draw_ship.c
@@ -12,71 +12,16 @@
 
 #include "../includes/fractol.h"
 
-static int	calc_color(double pr, double pi, t_cam pos)
-{
-	double	new_r;
-	double	new_i;
-	double	old_r;
-	double	old_i;
-	int		i;
-
-	new_r = 0;
-	new_i = 0;
-	i = 0;
-	while ((new_r * new_r + new_i * new_i) < 4.0 && i < pos.iter)
-	{
-		old_r = ft_fabs(new_r);
-		old_i = ft_fabs(new_i);
-		new_r = old_r * old_r - old_i * old_i + pr;
-		new_i = 2.0 * old_r * old_i + pi;
-		i++;
-	}
-	return (i);
-}
-
-static void	*draw_part(void *arg)
-{
-	t_fdf	*fdf;
-	int		x;
-	int		y;
-	double	pi;
-	double	pr;
-
-	fdf = ((t_th *)arg)->fdf;
-	y = (((t_th *)arg)->th < 2 ? 0 : fdf->p_win.sy / 2);
-	while (y < (((t_th *)arg)->th < 2 ? fdf->p_win.sy / 2 : fdf->p_win.sy))
-	{
-		x = (((t_th *)arg)->th % 2 ? fdf->p_win.sx / 2 : 0);
-		while (x < (((t_th *)arg)->th % 2 ? fdf->p_win.sx : fdf->p_win.sx / 2))
-		{
-			pr = x / fdf->cam.z - (fdf->cam.x + fdf->p_win.sx / 4) / fdf->cam.z * 2;
-			pi = y / fdf->cam.z - (fdf->cam.y + fdf->p_win.sy / 4) / fdf->cam.z * 2;
-			fill_pixel(fdf, x, y, calc_color(pr, pi, fdf->cam) * fdf->color);
-			x++;
-		}
-		y++;
-	}
-	pthread_exit(NULL);
-}
-
 void		draw_ship(t_fdf *fdf)
 {
-	pthread_t	threads[4];
-	t_th		pth[4];
-	int			x;
+	t_frcl tmp;
 
-	x = 0;
-	while (x < 4)
-	{
-		pth[x].fdf = fdf;
-		pth[x].th = x;
-		pthread_create(&threads[x], NULL, draw_part, &(pth[x]));
-		x++;
-	}
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
-	pthread_join(threads[2], NULL);
-	pthread_join(threads[3], NULL);
-	mlx_clear_window(fdf->mlx, fdf->win);
-	mlx_put_image_to_window(fdf->mlx, fdf->win, fdf->img, 0, 0);
+	tmp.iter = fdf->cam.iter;
+	tmp.camx = fdf->cam.x;
+	tmp.camy = fdf->cam.y;
+	tmp.camz = fdf->cam.z;
+	tmp.winsx = fdf->p_win.sx;
+	tmp.winsy = fdf->p_win.sy;
+	tmp.color = fdf->color;
+	gpu_calcul(tmp, fdf, SHIPDRAWER);
 }
