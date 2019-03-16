be549a35d489037f466cd71c5d8e16cd26ac6419
Maxence GRAB <magrab@e2r4p13.42.fr>
2019-03-16 15:29:13 +0100

    NORME

--- srcs/draw_julia.c
+++ srcs/draw_julia.c
@@ -14,8 +14,21 @@
 
 void				draw_julia(t_fdf *fdf)
 {
-	t_frcl tmp;
+	t_frcl	tmp;
+	char	draw[773];
 
+	ft_strcpy(draw, "typedef struct s_frcl{double camx;double camy;double camz;\
+double jx;double jy;int winsx;int winsy;int iter;int color;}t_frcl;\
+__kernel void color(__global const t_frcl *in, __global unsigned int *out)\
+{const uint index = get_global_id(0);const double x = index % in->winsx;\
+const double y = index / in->winsx;\
+const double pr = x / in->camz - (in->camx * 4.0 + in->winsx)/ (in->camz * 2);\
+const double pi = y / in->camz - (in->camy * 4.0 + in->winsy)/ (in->camz * 2);\
+double new_r;double new_i;double old_r;double old_i;int i;new_r = pr;\
+new_i = pi;i = 0;while ((new_r * new_r + new_i * new_i) < 18.0\
+&& i < in->iter){old_r = new_r;old_i = new_i;new_r = old_r * old_r\
+- old_i * old_i + in->jx / 100.0;new_i = 2.0 * old_r * old_i + in->jy / 100.0;\
+i++;}out[index] = i * in->color;}");
 	tmp.iter = fdf->cam.iter;
 	tmp.camx = fdf->cam.x;
 	tmp.camy = fdf->cam.y;
@@ -25,5 +38,5 @@ void				draw_julia(t_fdf *fdf)
 	tmp.color = fdf->color;
 	tmp.jx = fdf->cam.jx;
 	tmp.jy = fdf->cam.jy;
-	gpu_calcul(tmp, fdf, JULIADRAWER);
+	gpu_calcul(tmp, fdf, draw);
 }
--- srcs/draw_ship.c
+++ srcs/draw_ship.c
@@ -14,8 +14,21 @@
 
 void		draw_ship(t_fdf *fdf)
 {
-	t_frcl tmp;
+	t_frcl	tmp;
+	char	draw[792];
 
+	ft_strcpy(draw, "typedef struct s_frcl{double camx;double camy;double camz;\
+double jx;double jy;int winsx;int winsy;int iter;int color;}t_frcl;\
+__kernel void color(__global const t_frcl *in, __global unsigned int *out)\
+{const uint index = get_global_id(0);const double x = index % in->winsx;\
+const double y = index / in->winsx;\
+const double pr = x / in->camz - (in->camx * 4.0 + in->winsx)/ (in->camz * 2);\
+const double pi = y / in->camz - (in->camy * 4.0 + in->winsy)/ (in->camz * 2);\
+double new_r;double new_i;double old_r;double old_i;int i;new_r = 0;new_i = 0;\
+i = 0;while ((new_r * new_r + new_i * new_i) < 4.0 && i < in->iter){\
+old_r = (new_r < 0 ? -new_r : new_r);old_i = (new_i < 0 ? -new_i : new_i);\
+new_r = old_r * old_r - old_i * old_i + pr;\
+new_i = 2.0 * old_r * old_i + pi;i++;}out[index] = i * in->color;}");
 	tmp.iter = fdf->cam.iter;
 	tmp.camx = fdf->cam.x;
 	tmp.camy = fdf->cam.y;
@@ -23,5 +36,5 @@ void		draw_ship(t_fdf *fdf)
 	tmp.winsx = fdf->p_win.sx;
 	tmp.winsy = fdf->p_win.sy;
 	tmp.color = fdf->color;
-	gpu_calcul(tmp, fdf, SHIPDRAWER);
+	gpu_calcul(tmp, fdf, draw);
 }
